9bf909f8ddf433bed3e1eaa46cdb5db9acd47601
anup kumar <anupk0792@gmail.com>
2020-08-08 16:14:59 +0530

    inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemoproj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            System.Diagnostics.Trace.TraceInformation("This is Home method hit....");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            System.Diagnostics.Trace.TraceInformation("This is About method hit....");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            System.Diagnostics.Trace.TraceInformation("This is contact method hit....");
+            return View();
+        }
+    }
+}
